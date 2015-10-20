b9a519b21b9487dc864d38f6efa00dfd3ff5b7bc
Hackbright Student <info@hackbrightacademy.com>
2015-10-20 16:29:39 -0700

    movie load is working

--- seed.py
+++ seed.py
@@ -35,6 +35,12 @@ def load_users():
     # Once we're done, we should commit our work
     db.session.commit()
 
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 def load_movies():
     """Load movies from u.item into database."""
@@ -49,37 +55,43 @@ def load_movies():
         row_list = row.split("|")
 
         #Check the ID
-        if type(row_list[0]) != type(5):
-            print type(row_list[0])
-            print type(5)
-            print "Are we failling on line 53?"
-            continue ## there is a way to tell it to skip this iternation and continue for loop
+        if is_number(row_list[0]) == True:
+            movie_id = int(row_list[0])
         else:
-            movie_id = row_list[0]
+            print "That ain't no numba"
+            continue
+
+        # if int(row_list[0]):
+        #     movie_id = int(row_list[0])
+        #     print "the movie id: ", movie_id
+        # else:
+        #     continue
+
 
         #Check the datetime format
-        print "this is the movie date before checking ", row_list[2]
+
         if len(row_list[2]) != 11:
             continue## there is a way to tell it to skip this iternation and continue for loop
-            print "the movie date did not pass test"
         else:
             released_at = row_list[2]
 
-        #Check if the movie title and date is already in the database
-        check_movie = Movie.query.filter(Movie.movie_title == row_list[1]).first()
-        if check_movie != None:
-            continue## there is a way to tell it to skip this iternation and continue for loop
-            print "Move already in here ", row_list[1]
-        else:
-            movie_title = row_list[1]
-            movie_title = movie_title[:-7]
-
+        #TODO: Check if the movie title and date is already in the database
+            # check_movie = Movie.query.filter(Movie.movie_title == row_list[1]).first()
+            # if check_movie != None:
+            #     continue## there is a way to tell it to skip this iternation and continue for loop
+            #     print "Move already in here ", row_list[1]
+            # else:
+            #     movie_title = row_list[1]
+            #     movie_title = movie_title[:-7]
+        
+        movie_title = row_list[1]
+        movie_title = movie_title[:-7]
+        
         imdb_url = row_list[4]
-        print "movie title:", movie_title
-        print "released at:", released_at
+        
 
         released_datetime = datetime.strptime(released_at, "%d-%b-%Y")              #made a datetime object
-        print "relased datatime:", released_datetime
+        
         movie = Movie(movie_id=movie_id, movie_title=movie_title, released_datetime=released_datetime, imdb_url=imdb_url)
 
         db.session.add(movie)
